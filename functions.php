c66931edf3d6a4ecaad5b47147571be7bb38cd48
Tyler Pierce <>
2026-05-25 15:53:20 -0400

    Initial RRH Divi child theme setup

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function rrh_child_assets() {
+
+    wp_enqueue_style(
+        'rrh-child-style',
+        get_stylesheet_directory_uri() . '/assets/css/style.min.css',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/css/style.min.css')
+    );
+
+    wp_enqueue_script(
+        'rrh-child-script',
+        get_stylesheet_directory_uri() . '/assets/js/app.min.js',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/js/app.min.js'),
+        true
+    );
+}
+
+add_action('wp_enqueue_scripts', 'rrh_child_assets');
---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+import './components/mega-menu';
